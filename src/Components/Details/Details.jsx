418fa71c5db485eaab03c3535468f8acc7146623
Michael <michael.pecora.92@gmail.com>
2024-02-29 17:44:43 +0100

    update

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Rating from 'react-rating-stars-component';
+import Rating from "react-rating-stars-component";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import axios from "axios";
@@ -40,58 +40,69 @@ const Details = () => {
       }
     };
     getByAsin();
-    
   }, [dispatch, elementId, asin]);
 
   if (!bookDetails) return <div>Loading...</div>;
 
   return (
     <>
-      <MyNav isSearchActive={false}/>
+      <MyNav isSearchActive={false} />
       <Row className="mt-2 ">
         <Col className="m-2">
-      <Card>
-      <Card.Img variant="bottom" src={bookDetails.img} />
-        <Card.Body>
-        
-          <ListGroup className="list-group-flush text-center">
-            <ListGroup.Item>
-              <strong>Title: <br></br>{bookDetails.title}</strong>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Category:<br></br></strong> {bookDetails.category}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Price:<br></br></strong> {bookDetails.price}$
-            </ListGroup.Item>
-          </ListGroup>
-          <hr />
-        </Card.Body>
+          <Card>
+            <Card.Img variant="bottom" src={bookDetails.img} />
+            <Card.Body>
+              <ListGroup className="list-group-flush text-center">
+                <ListGroup.Item>
+                  <strong>
+                    Title: <br></br>
+                    {bookDetails.title}
+                  </strong>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>
+                    Category:<br></br>
+                  </strong>{" "}
+                  {bookDetails.category}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>
+                    Price:<br></br>
+                  </strong>{" "}
+                  {bookDetails.price}$
+                </ListGroup.Item>
+              </ListGroup>
+              <hr />
+            </Card.Body>
 
-        <Card.Header as='h6' className="text-muted text-center">The ID Code of the book that you've selected is: {bookDetails.asin}</Card.Header>
-      </Card>
-      </Col>
-      <Col className="d-flex flex-column align-items-center justify-content-center">
-
-      <h2>Reviews:</h2>
-        {comments.map((comment) => (
-          
+            <Card.Header as="h6" className="text-muted text-center">
+              The ID Code of the book that you've selected is:{" "}
+              {bookDetails.asin}
+            </Card.Header>
+          </Card>
+        </Col>
+        <Col className="d-flex flex-column align-items-center justify-content-center">
+          <h2>Reviews:</h2>
+          {comments.map((comment) => (
             <Card key={comment._id} style={{ width: "25rem" }} className="m-1">
               <Card.Body>
-                <Card.Title>User Email: <br></br> {comment.author}</Card.Title>
+                <Card.Title>
+                  User Email: <br></br> {comment.author}
+                </Card.Title>
                 <hr />
                 <Rating
-              value={comment.rate}
-              count={5}
-              edit={false}
-              size={25}
-              activeColor="#ffd700"
-              
-            />
-                <Card.Text><strong>Comment:</strong> <br></br> {comment.comment}</Card.Text>
+                  value={comment.rate}
+                  count={5}
+                  edit={false}
+                  size={25}
+                  activeColor="#ffd700"
+                />
+                <Card.Text>
+                  <strong>Comment:</strong> <br></br> {comment.comment}
+                </Card.Text>
                 <Card.Subtitle className="mb-2 text-muted">
-                <hr />
-                <strong>Published On:</strong>{" "}
+                  <hr />
+                  <strong>Published On:</strong>{" "}
                   {new Date(comment.createdAt).toLocaleDateString("en-US", {
                     year: "numeric",
                     month: "long",
@@ -100,8 +111,7 @@ const Details = () => {
                 </Card.Subtitle>
               </Card.Body>
             </Card>
-          
-        ))}
+          ))}
         </Col>
       </Row>
 
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faFacebookF, faInstagram, faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
